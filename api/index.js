f775461d2fd17c49d3bf02e394b28816a7e8322e
Khantdotcom <kokhanthtay@gmail.com>
2024-12-11 22:45:19 +0630

    mongodb connected

--- api/index.js
+++ api/index.js
@@ -5,8 +5,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect('mongodb+srv://blog:<db_password>@cluster0.elthj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
 app.post('/register',(req,res)=>{
     const {username,password} = req.body;
     res.json({requestData:{username,password}})
-})
+});
+
 app.listen(4000);
+
