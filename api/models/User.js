bffce43360e4ad86ab995bb0ad75db1139511a01
Khantdotcom <kokhanthtay@gmail.com>
2024-12-11 23:19:35 +0630

    Fixed issues in index.js

--- api/models/User.js
+++ api/models/User.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
-;
+const {Schema, model} = mongoose;
+
 const UserSchema = new Schema({
-    username: {type:String, require:true, min:4, unique:true},
-    password: {type:String, require:true}
-}
-);
+  username: {type: String, required: true, min: 4, unique: true},
+  password: {type: String, required: true},
+});
 
 const UserModel = model('User', UserSchema);
 
