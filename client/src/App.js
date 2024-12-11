3bd1925e71667791300ee7352931cc0446a0713a
Khantdotcom <kokhanthtay@gmail.com>
2024-12-11 20:11:32 +0630

    Project started

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './header';
+import Post from './post>;
+
+
+function App() {
+  return (
+    <main>
+      <Header/>
+      <Post/>
+      <Post/>
+      <Post/>
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/post.js
@@ -0,0 +1,17 @@
+export default function Post(){
+    return(
+        <div className="post">
+        <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2024/11/wordpress-automattic-v2.jpg?resize=1200,800" alt="" />
+        </div>
+          <div className="content">
+          <h2>Publish your passions, your way          </h2>
+          <p className="info">
+            <a href="" className="author">Khant Htay</a>
+            <time>2024-11-12 29:21</time>
+          </p>
+          <p className="summary">Get paid for your hard work. Google AdSense can automatically display relevant targeted ads on your blog so that you can earn income by posting about your passion.</p>
+          </div>
+        </div>
+    );
+}
