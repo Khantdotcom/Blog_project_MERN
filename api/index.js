ba694b5e4aa6383c85975f2cd81036b50c3ce34d
Khantdotcom <kokhanthtay@gmail.com>
2024-12-12 18:29:50 +0630

    fixed module import issue

--- api/index.js
+++ api/index.js
@@ -2,16 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
+const User = require('./models/User');
 
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://blog:12345@cluster0.elthj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
-app.post('/register',(req,res)=>{
+app.post('/register',async (req,res)=>{
     const {username,password} = req.body;
-    console.log({username});
-    res.json({requestData:{username,password}})
+    try{
+        const userDoc = await User.create({username,password});
+        res.json(userDoc);
+    } catch(e) {
+        res.status(400).json(e);
+    }
 });
 app.listen(4000);
 
--- api/models/User.js
+++ api/models/User.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
-;
+
 const UserSchema = new Schema({
     username: {type:String, require:true, min:4, unique:true},
     password: {type:String, require:true}
 }
 );
 
-const UserModel = model('User', UserSchema);
+const UserModel = mongoose.model('User', UserSchema);
 
 module.exports = UserModel;
